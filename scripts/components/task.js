acacc78130f1dc58f8b165662bc0d0f4f7d523bb
desmoris <damianesmoris@gmail.com>
2025-06-27 02:25:50 -0300

    Compomente tarea

---
+++ scripts/components/task.js
@@ -0,0 +1,45 @@
+export class TaskElement extends HTMLElement {
+    constructor(task) {
+        super();
+        task.startDate = new Date(task.start_date);
+        task.dueDate = new Date(task.due_date)
+        delete task.start_date;
+        delete task.due_date;
+        this.task = task;
+    }
+
+    connectedCallback() {
+        const mainContainer = document.createElement('section');
+        const article = document.createElement('article');
+        mainContainer.className = 'container';
+
+        const header = document.createElement('header');
+
+        const title = document.createElement('h1');
+        title.textContent = this.task.title;
+
+        const categoriesCont = document.createElement('div');
+        categoriesCont.innerHTML = this.task.categories.map(({name}) => `<span>${name}</span>`);
+
+        const dateText = document.createElement('i');
+        dateText.textContent =
+            this.task.startDate.toLocaleString()
+            +  ' - '
+            + this.task.dueDate.toLocaleString();
+
+        header.appendChild(title);
+        header.appendChild(categoriesCont);
+        header.appendChild(dateText);
+
+        const description = document.createElement('p');
+        description.textContent = this.task.description;
+
+        article.appendChild(header);
+        article.appendChild(description);
+        mainContainer.appendChild(article);
+        this.appendChild(mainContainer);
+    }
+}
+
+
+customElements.define('task-info', TaskElement) 
--- scripts/task.js
+++ scripts/task.js
@@ -1,73 +1,20 @@
+import { CommentsElement } from './components/comments.js';
 import './components/nav-bar.js';
-import { FETCH_DEFUALT_HEADERS, FETCH_HEADER_WITH_AUTH, TASK_API_URL } from "./modules/constants.js";
-import { getUsers, userData } from './modules/userMangement.js';
+import { TaskElement } from './components/task.js';
+import { FETCH_DEFUALT_HEADERS, TASK_API_URL } from "./modules/constants.js";
 
-
-const HTML_ELEMENTS = Object.freeze({
-    TASK_CONTAINER: document.getElementById('taskContainer'),
-    COMMENTS_CONTAINER: document.getElementById('commentsContainer'),
-    COMMENT_FORM: document.getElementById('commentForm')
-});
-
-const USER_ID_MAPPING = new Map();
 const URL_HASH = window.location.hash;
 const TASK_ID = URL_HASH.substring(1);
 
 if (!TASK_ID || isNaN(TASK_ID))
     document.location = '/';
 
-function parseComments(comments) {
-}
-
-function getTaskDetails({id, title, author_id: _authorId, description, start_date: startDate, due_date: dueDate, categories = [], comments}) {
-    return `
-    <article>
-    <header>
-        <h1>${id}: ${title}</h1>
-        <div>${categories.map(({name}) => `<span>${name}</span>`)}</div>
-        <i>${new Date(startDate).toLocaleString()} - ${new Date(dueDate).toLocaleString()}</i>
-    </header>
-    <p>${description}</p>
-    <footer>
-        ${comments.reduce((acc, {author_id: authorId, body}) =>
-        `${acc}<article>${USER_ID_MAPPING.get(authorId)['name']}: ${body}</article>`, '')}
-    </footer>
-    </article>
-    `;
-}
-
-
-getUsers().then(users =>
-    users.forEach(user => USER_ID_MAPPING.set(user.id+'', user))
-)
-.then(() =>
-    fetch(TASK_API_URL + `/task/${TASK_ID}`, {
-        headers: FETCH_DEFUALT_HEADERS,
-    })
-    .then(res => res.ok ? res.json() : new Error())
-    .then(task =>  
-        HTML_ELEMENTS.TASK_CONTAINER.innerHTML += getTaskDetails(task)
-    )
-    .catch(err => console.error(err))
-)
-.catch(error => alert(error));
-
-
-
-HTML_ELEMENTS.COMMENT_FORM.onsubmit = (e) => {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(HTML_ELEMENTS.COMMENT_FORM));
-    data.task_id = TASK_ID;
-    data.author_id = userData()['id'];
-    fetch(TASK_API_URL + '/comment', {
-        method: "POST",
-        headers: FETCH_HEADER_WITH_AUTH(),
-        body: JSON.stringify(data)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject())
-    .then(() => location.reload())
-    .catch(error => {
-        console.error(error);
-        HTML_ELEMENTS.COMMENT_FORM.reset();
-    });
-}
+fetch(TASK_API_URL + `/task/${TASK_ID}`, {
+    headers: FETCH_DEFUALT_HEADERS,
+})
+.then(res => res.ok ? res.json() : new Error())
+.then(task => {
+    document.body.appendChild(new TaskElement(task))
+    document.body.appendChild(new CommentsElement(TASK_ID, task.comments))
+})
+.catch(err => console.error(err))
