012544591c1bad0d80f6a189e93a5ce39e4732fb
desmoris <damianesmoris@gmail.com>
2025-07-07 05:41:22 -0300

    Se quitan imports del index

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,2 @@
 import './components/nav-bar.js';
 import './components/task-table.js';
-import { FETCH_DEFUALT_HEADERS, TASK_API_URL } from "./modules/constants.js";
-
-
-/*
-function getTaskTableRow({id, title, author_id: authorId, start_date: startDate, due_date: dueDate, categories}) {
-    return `<tr>
-        <td><a href="/task.html#${id}">${id}</a></td>
-        <td>${title}</td>
-        <td>${authorId}</td>
-        <td>${}</td>
-        <td>${new Date(dueDate).toLocaleString()}</td>
-        <td>${}</td>
-    </tr>`;
-}
-
-*/
